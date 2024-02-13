273044befaad5e9e9dddbb5df51faffd582080d2
kaleman18 <kylealeman18@gmail.com>
2024-02-12 17:17:15 -0800

    lab done

---
+++ snacks/urls.py
@@ -0,0 +1,11 @@
+# associate url paths with views
+from django.urls import path
+
+from .views import AboutPageView, HomePageView
+
+
+urlpatterns = [
+    path('', HomePageView.as_view(), name='home'),
+    path('about', AboutPageView.as_view(), name='about')
+
+]
